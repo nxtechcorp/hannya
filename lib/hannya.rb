26aa763063cabcef3255e6bc601da7c24e7d36c1
Jeremy Cronk <jcronk@nxtechcorp.com>
2014-11-18 18:47:03 -0500

    First version with passing tests

--- lib/hannya.rb
+++ lib/hannya.rb
@@ -1,4 +1,3 @@
-require 'hannya/accessor'
 require 'hannya/address_converter'
 
 # A data access layer module.  A hannya in Japan is a female demon
@@ -7,23 +6,122 @@ require 'hannya/address_converter'
 # allowing you to access it in a simple way.
 module Hannya
 
-  def self.Accessor(source, opts)
-    converter = opts[:address_converter]
-    args = converter ? [source, DataAddressConverter.create(converter)] : [source]
-    case source.class.to_s
-    when /^Nokogiri::XML/ then XMLDataAccessor.new(*args)
-    when /Hash/, /Array/ then DataAccessor.new(*args)
-    else raise "Unknown source type #{source.class}"
+  # Put the hannya mask over your data.
+  # @param source [Nokogiri::XML::Node, Hash, Array] the data source
+  # @param opts [Hash] the options
+  # @option opts [Boolean] :force_array whether to always return an array
+  # @option opts [Hannya::DataAddressConverter] :converter the address converter object.
+  #   This is what converts symbols into strings that will return a result from the data
+  #   source.
+  def self.Mask(source, opts={})
+    case source
+    when Nokogiri::XML::Node then XMLAccessor.new(source, opts)
+    when Hash then HashAccessor.new(source, opts)
+    else raise TypeError, "Unknown source type: #{source.class}"
     end
   end
 
-  def self.Finder(source, opts)
-    converter = opts[:address_converter]
-    args = converter ? [source, DataAddressConverter.create(converter)] : [source]
-    case source.class.to_s
-    when /^Nokogiri::XML/ then XMLDataFinder.new(*args)
-    when /Hash/, /Array/ then DataFinder.new(*args)
-    else raise "Unknown source type #{source.class}"
+  class Mask
+    # @!attribute [r] source [Nokogiri::XML::Node, Hash, Array] the data source
+    attr_reader :source
+
+    def initialize(source, opts={})
+      @opts_hash = opts
+      @source = source
+      @converter = opts.fetch(:converter) { default_converter }
+      @force_array = opts[:force_array]
+    end
+
+    def create_method(method_name, query_result)
+      define_singleton_method(method_name) do
+        create_result_list(query_result)
+      end
+    end
+
+    def [](key)
+      query_result = execute_query(key)
+      create_result_list(query_result)
+    end
+
+    # TODO: This is ugly
+    def create_result_list(query_result)
+      return create_result_list_from_hash(query_result) if query_result.is_a?(Hash)
+      create_result_list_from_array(query_result)
+    end
+
+    def create_result_list_from_array(query_result)
+      result_list = Array(query_result).map do |node|
+        get_text_or_node(node)
+      end
+      scalarize(result_list)
+    end
+
+    def create_result_list_from_hash(query_result)
+      query_result.each_with_object({}) do |(key, value), result_list|
+        result_list[key] = get_text_or_node(value)
+      end
+    end
+
+    def list(*keys)
+      keys.map do |key|
+        send key
+      end
+    end
+
+    def respond_to_missing(method_name, include_private)
+      !execute_query(@converter.convert method_name).empty?
+    end
+
+    def method_missing(method_name, *args, &block)
+      result = execute_query(@converter.convert method_name)
+      return scalarize(Array(nil)) if result.empty?
+      create_method(method_name, result)
+      send(method_name)
+    end
+
+    def scalarize(array)
+      return array if array.count > 1 || @force_array
+      array.first
+    end
+
+    def wrap_data(node)
+      self.class.new(node, @opts_hash)
+    end
+  end
+
+  class XMLAccessor < Mask
+    def execute_query(xpath)
+      @source.xpath(xpath)
+    end
+
+    def get_text_or_node(node)
+      if node.element_children.empty?
+        node.text
+      else
+        wrap_data(node)
+      end
+    end
+
+    def default_converter
+      DataAddressConverter.create(:camel)
+    end
+  end
+
+  class HashAccessor < Mask
+    def execute_query(key)
+      return Array(@source[key]) unless @source[key].is_a?(Hash)
+      @source[key]
+    end
+
+    def get_text_or_node(node)
+      case node
+      when String, Array then node
+      when Hash then wrap_data(node)
+      end
+    end
+
+    def default_converter
+      DataAddressConverter.create(:upper)
     end
   end
 
--- spec/lib/hannya_spec.rb
+++ spec/lib/hannya_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
-require 'nokogiri'
 require 'hannya'
+require 'nokogiri'
+
+include Hannya
 
 xmlstring = <<-EOT
 <Root>
@@ -25,6 +27,7 @@ xmlstring = <<-EOT
   </Policy>
 </Root>
 EOT
+
 nested_data = { 'THUD' => 'PLUGH',
                 'BLETCH' => { 'SNORK' => 'BORK',
                               'CORGE' => 'BAZ',
@@ -32,23 +35,133 @@ nested_data = { 'THUD' => 'PLUGH',
                 'XYZZY' => %w(GARPLY FRED WALDO FUBAR BAR),
                 'GRAULT' => 'PLOVER',
                 'QUUX' => 'BAZ' }
+RSpec.describe 'a data accessor object' do
 
-RSpec.describe "the accessor factory" do
+  context 'when the source data is a simple hash' do
 
-  context "when the data source is XML" do
-    xml = Nokogiri::XML(xmlstring).at_xpath('Root/Policy')
+    subject(:acc) do
+      HashAccessor.new('THUD' => 'PLUGH',
+                       'XYZZY' => 'PLOVER',
+                       'QUUX' => 'BAZ')
+    end
 
-    it "returns an XML accessor" do
-      ac = Hannya::Accessor(xml, address_converter: :camel)
-      expect(ac[:policy_number]).to eq '123456'
+    it 'allows access to data as an instance method' do
+      expect(acc.thud).to eq 'PLUGH'
+    end
+
+    it 'allows access to data by string' do
+      expect(acc['XYZZY']).to eq 'PLOVER'
+    end
+
+    it 'returns a list of values when given a list' do
+      expect(acc.list(:quux, :thud, :xyzzy)).to eq %w(BAZ PLUGH PLOVER)
+    end
+
+    context 'when a nonexistent value is specified' do
+
+      context 'and force_array is not set' do
+        it 'returns nil' do
+          expect(acc.grault).to be_nil
+        end
+      end
+
+      context 'and force_array is set' do
+        acc_with_fa = HashAccessor.new({ 'THUD' => 'PLUGH',
+                                         'XYZZY' => 'PLOVER',
+                                         'QUUX' => 'BAZ' }, force_array: true)
+        it 'returns an empty array' do
+          expect(acc_with_fa.grault).to eq []
+        end
+      end
+    end
+  end
+
+  context 'when the source data is a complex hash' do
+    subject(:acc) { HashAccessor.new(nested_data) }
+
+    context 'when the result is a data structure' do
+      it 'returns the result' do
+        expect(acc.bletch).to eq nested_data['BLETCH']
+      end
+    end
+
+    context 'when the input is a list' do
+
+      context 'and all the items are data structures' do
+        it 'returns the items' do
+          expect(acc.list(:bletch, :xyzzy)).to eq [nested_data['BLETCH'],
+                                                   nested_data['XYZZY']]
+        end
+      end
+
+      context 'and all the items are strings' do
+        it 'returns a list of strings' do
+          expect(acc.list(:thud, :grault, :quux)).to eq %w(PLUGH PLOVER BAZ)
+        end
+      end
+
     end
 
   end
 
-  context "when it is a hash" do
+  context 'when the source data is XML' do
+    subject(:acc) do
+      xml = Nokogiri::XML(xmlstring)
+      XMLAccessor.new(xml.at_xpath('Root/Policy'))
+    end
 
-    it "returns a data accessor"
+    it 'allows access to data using method calls' do
+      expect(acc.policy_number).to eq '123456'
+    end
 
+    it 'allows access to data by xpath' do
+      expect(acc['Address[1]/Street']).to eq '123 Mockingbird Ln.'
+    end
+
+    it 'returns a list of values when given a list' do
+      expect(acc.list(:policy_number, :transaction_type, :insured_name)).to eq \
+        ['123456', 'XLC', 'Joe Smith']
+    end
+
+    context 'when the result is a single node' do
+
+      context 'and force_array is not set' do
+
+        it 'returns the node text' do
+          expect(acc.policy_number).to eq '123456'
+        end
+
+      end
+
+      context 'and force_array is set' do
+        subject(:acc) do
+          xml = Nokogiri::XML(xmlstring)
+          XMLAccessor.new(xml.at_xpath('Root/Policy'), force_array: true)
+        end
+
+        it 'returns an array' do
+          expect(acc.policy_number).to eq %w(123456)
+        end
+
+      end
+
+    end
+
+    context 'when the result is a nodeset' do
+
+      it 'returns a data accessor' do
+        expect(acc['Address[1]']).to be_an_instance_of(XMLAccessor)
+      end
+
+    end
+
+    context 'when the input is an array' do
+
+      it 'returns an array of data accessors' do
+        expect(acc.list(:insured_name, :transaction_type)).to be_an_instance_of(Array)
+      end
+
+    end
   end
 
 end
